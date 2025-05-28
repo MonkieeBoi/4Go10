534bb63827780f18606652a076bc31d61114b70a
MonkieeBoi <danny.pham26425@gmail.com>
2025-05-28 20:40:41 +1000

    feat: timer

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -9,6 +13,19 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("4Go10")
 
-	w.SetContent(widget.NewLabel("Hi"))
+	timer := widget.NewProgressBar()
+	timer.TextFormatter = func() string {return ""}
+
+	go func() {
+		secs := 10.0
+		for i := 1.0; i >= 0; i -= 0.001 / secs {
+			time.Sleep(time.Millisecond * 1)
+			fyne.Do(func() {
+				timer.SetValue(i)
+			})
+		}
+	}()
+
+	w.SetContent(container.NewVBox(timer))
 	w.ShowAndRun()
 }
