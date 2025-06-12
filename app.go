e1a1509dd14d47dc50fb64274ae02a94b109a546
MonkieeBoi <danny.pham26425@gmail.com>
2025-06-12 13:01:33 +1000

    feat: basic application flow

--- app.go
+++ app.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"math/rand/v2"
+	"strconv"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -14,6 +18,29 @@ func app() *fyne.Container {
 	title.Alignment = fyne.TextAlignCenter
 
 	win := container.New(layout.NewVBoxLayout(), title)
+
+	main_loop := func() {
+		win.RemoveAll()
+		num := strconv.Itoa(rand.IntN(100))
+		win.Add(widget.NewLabel(num))
+		timer, done := timerWidget(time.Duration(1 * time.Second))
+		win.Add(timer)
+		_ = <-done
+		win.RemoveAll()
+		entry := widget.NewEntry()
+		win.Add(entry)
+		entry.OnSubmitted = func(s string) {
+			win.RemoveAll()
+			if s != num {
+				win.Add(widget.NewLabel("Wrong"))
+			} else {
+				win.Add(widget.NewLabel("Correct"))
+			}
+		}
+	}
+
+	start_btn := widget.NewButton("Start", func() { go main_loop() })
+	win.Add(start_btn)
 
 	return win
 }
